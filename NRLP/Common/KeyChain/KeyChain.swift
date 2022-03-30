9fe3bbadfcbc5c184223d9a7bf2bbb7ffe83c818
Muhammad Shahid Shakeel <shahid.shakeel@systemsltd.com>
2022-03-30 15:08:45 +0500

    device id persistence in keychain

--- NRLP/Common/KeyChain/KeyChain.swift
+++ NRLP/Common/KeyChain/KeyChain.swift
@@ -16,9 +16,10 @@ class KeyChain {
     
     class func save(key: String, data: Data) -> OSStatus {
         let query = [
-            kSecClass as String       : kSecClassGenericPassword as String,
-            kSecAttrAccount as String : key,
-            kSecValueData as String   : data ] as [String : Any]
+            kSecClass as String: kSecClassGenericPassword as String,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ] as [String: Any]
 
         SecItemDelete(query as CFDictionary)
 
@@ -27,12 +28,13 @@ class KeyChain {
 
     class func load(key: String) -> Data? {
         let query = [
-            kSecClass as String       : kSecClassGenericPassword,
-            kSecAttrAccount as String : key,
-            kSecReturnData as String  : kCFBooleanTrue!,
-            kSecMatchLimit as String  : kSecMatchLimitOne ] as [String : Any]
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ] as [String: Any]
 
-        var dataTypeRef: AnyObject? = nil
+        var dataTypeRef: AnyObject?
 
         let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
 
@@ -55,14 +57,35 @@ class KeyChain {
 class Defaults {
     
     static private let SDRPNamePrefix: String = "SohniDhartiRemittanceProgram"
+    static private let keyPersistantUUID = SDRPNamePrefix + "persistanUUID"
     
-    static var persistanUUID : String?{
-        set{
-            UserDefaults.standard.set(newValue, forKey: SDRPNamePrefix + "persistanUUID")
-            UserDefaults.standard.synchronize()
+    static var persistanUUID: String? {
+        get {
+            var deviceId =  UserDefaults.standard.string(forKey: keyPersistantUUID)
+
+            if deviceId == nil {
+                // Get Device Id from Keychain
+                if let data = KeyChain.load(key: keyPersistantUUID) {
+                    deviceId = String(data: data, encoding: .utf8)
+                    
+                    UserDefaults.standard.set(deviceId, forKey: keyPersistantUUID)
+                    UserDefaults.standard.synchronize()
+                }
+            }
+            
+            return deviceId
         }
-        get{
-            return UserDefaults.standard.string(forKey: SDRPNamePrefix + "persistanUUID")
+        set {
+            UserDefaults.standard.set(newValue, forKey: keyPersistantUUID)
+            UserDefaults.standard.synchronize()
+            
+            // Update in Keychain
+            let status = KeyChain.save(key: keyPersistantUUID, data: Data((newValue ?? "").utf8))
+            if status == noErr {
+                print("device id saved in keychain")
+            } else {
+                print("failed to save device id in keychain")
+            }
         }
     }
 }
@@ -78,4 +101,3 @@ extension Data {
         return self.withUnsafeBytes { $0.load(as: T.self) }
     }
 }
-
